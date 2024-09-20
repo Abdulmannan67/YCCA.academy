a7145e39e9dfd2d62a436abccadf6488c59cb571
Abdul Mannan <am2595171@gmail.com>
2024-09-20 09:17:38 +0530

    env added

--- src/components/payment/intent-payment.js
+++ src/components/payment/intent-payment.js
@@ -1,6 +1,6 @@
 import Stripe from "stripe";
 
-const stripe = new Stripe( process.env.STRIPE_API_KEY);
+const stripe = new Stripe( process.env.NEXT_STRIPE_API_KEY);
 
 export default async (req, res) => {
   const paymentIntent = await stripe.paymentIntents.create({
